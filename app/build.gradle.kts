ab872093e9ede4b12bbeba08108104ba23680bfd
GlassOnTin <GlassOnTin@users.noreply.github.com>
2026-08-07 00:34:30 +0100

    release: v5.86.50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "sh.haven.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 758
-        versionName = "5.86.49"
+        versionCode = 759
+        versionName = "5.86.50"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
